525547cee89759fd6a20bd8dc7353f135b2ecd0b
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2017-02-23 19:40:03 -0300

    Single page application.

--- app-base/front-end/js/main.js
+++ app-base/front-end/js/main.js
@@ -1,1 +1,17 @@
-var app = angular.module('SistemaAcademico', []);
+var app = angular
+    .module('SistemaAcademico', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/alunoCadastro', {
+                templateUrl: 'aluno.html',
+                controller: 'AlunoController'
+            })
+            .when('/turmaCadastro', {
+                templateUrl: 'turma.html',
+                controller: 'TurmaController'
+            })
+            .when('/disciplinaCadastro', {
+                templateUrl: 'disciplina.html',
+                controller: 'DisciplinaController'
+            });
+    });
